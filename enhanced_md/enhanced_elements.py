0908a1dd916204e9781a01d90c38135da6b5efac
Pau Tarragó Navarra <68395147+pTarNav@users.noreply.github.com>
2024-03-05 12:06:02 +0100

    Implementation at COntent and Hyperlink level

--- enhanced_md/enhanced_elements.py
+++ enhanced_md/enhanced_elements.py
@@ -1,5 +1,6 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import List
+import re
 
 
 class Content:
@@ -12,109 +13,133 @@ class Content:
 		self.string: str = string
 
 		# Font style properties
-		self.italic: bool = italic
-		self.bold: bool = bold
-		self.underline: bool = underline
-		self.strike: bool = strike
-		self.superscript: bool = superscript
-		self.subscript: bool = subscript
+		self.font_style = {
+			"italic": italic, "bold": bold, "underline": underline, "strike": strike,
+			"superscript": superscript, "subscript": subscript
+		}
 
 	def __repr__(self):
-		return (f"string text: {repr(self.string)}"
-		        f"\t(italic: {self.italic}, bold: {self.bold}, underline: {self.underline}, strike: {self.strike}, "
-		        f"superscript: {self.superscript}, subscript: {self.subscript})")
+		return f"string text: {repr(self.string)}\t(font style: {self.font_style})"
 
 
 class BaseElement(ABC):
 	def __init__(
-			self, content: List[Content], style: str,
-			parent_element=None, children_elements=None, previous_element=None, next_element=None
+			self, content: List[Content]
 	):
 		#
 		self.content: List[Content] = content
-		self.style: str = style
+		self.text: str = self._construct_text_from_content()
 
-		# Graph relations
-		self.parent: BaseElement | None = parent_element
-		self.children: List[BaseElement] | None = children_elements
-		self.previous: BaseElement | None = previous_element
-		self.next: BaseElement | None = next_element
-
-		#
-		self.text: str = ""
-
-
-class Heading(BaseElement):
-	def __init__(
-			self,
-			# BaseElement attributes
-			content: List[Content], style: str,
-			# Heading specific attributes
-			# BaseElement attributes (with default)
-			parent_element: BaseElement | None = None, children_elements: List[BaseElement] | None = None,
-			previous_element: BaseElement | None = None, next_element: BaseElement | None = None
-			# Heading specific attributes (with default)
-	):
-		# BaseElement initialization
-		super().__init__(
-			content=content, style=style,
-			parent_element=parent_element, children_elements=children_elements,
-			previous_element=previous_element, next_element=next_element
-		)
-
-
-class Paragraph(BaseElement):
-	def __init__(
-			self,
-			# BaseElement attributes
-			content: List[Content], style: str,
-			# Paragraph specific attributes
-			# BaseElement attributes (with default)
-			parent_element: BaseElement | None = None, children_elements: List[BaseElement] | None = None,
-			previous_element: BaseElement | None = None, next_element: BaseElement | None = None
-			# Paragraph specific attributes (with default)
-	):
-		# BaseElement initialization
-		super().__init__(
-			content=content, style=style,
-			parent_element=parent_element, children_elements=children_elements,
-			previous_element=previous_element, next_element=next_element
-		)
-
-
-class Table(BaseElement):
-	def __init__(
-			self,
-			# BaseElement attributes
-			content: List[Content], style: str,
-			# Table specific attributes
-			# BaseElement attributes (with default)
-			parent_element: BaseElement | None = None, children_elements: List[BaseElement] | None = None,
-			previous_element: BaseElement | None = None, next_element: BaseElement | None = None
-			# Table specific attributes (with default)
-	):
-		# BaseElement initialization
-		super().__init__(
-			content=content, style=style,
-			parent_element=parent_element, children_elements=children_elements,
-			previous_element=previous_element, next_element=next_element
-		)
+	@abstractmethod
+	def _construct_text_from_content(self):
+		pass
 
 
 class Hyperlink(BaseElement):
 	def __init__(
-			self,
-			# BaseElement attributes
-			content: List[Content], style: str,
-			# Hyperlink specific attributes
-			# BaseElement attributes (with default)
-			parent_element: BaseElement | None = None, children_elements: List[BaseElement] | None = None,
-			previous_element: BaseElement | None = None, next_element: BaseElement | None = None
-			# Hyperlink specific attributes (with default)
+			self, content: List[Content], address: str, fragment: str
 	):
 		# BaseElement initialization
+		super().__init__(content=content)
+
+		#
+		if len(address):
+			self.link = address
+			self.type = "url"
+		elif len(fragment):
+			self.link = fragment
+			self.type = "jump"
+		else:
+			self.link = ""
+			self.type = None
+
+	def _construct_text_from_content(self):
+		return ""
+
+
+class DirectedElement(BaseElement):
+	def __init__(
+			self, content: List[Content], style: str,
+			parent_element=None, children_elements=None, previous_element=None, next_element=None
+	):
+		# BaseElement initialization
+		super().__init__(content=content)
+
+		#
+		self.style: str = style
+
+		# Graph relations
+		self.parent: DirectedElement | None = parent_element
+		self.children: List[DirectedElement] | None = children_elements
+		self.previous: DirectedElement | None = previous_element
+		self.next: DirectedElement | None = next_element
+
+	@abstractmethod
+	def _construct_text_from_content(self):
+		return ""
+
+
+class Heading(DirectedElement):
+	def __init__(
+			self,
+			# DirectedElement attributes
+			content: List[Content], style: str,
+			# Heading specific attributes
+			# DirectedElement attributes (with default)
+			parent_element: DirectedElement | None = None, children_elements: List[DirectedElement] | None = None,
+			previous_element: DirectedElement | None = None, next_element: DirectedElement | None = None
+			# Heading specific attributes (with default)
+	):
+		# DirectedElement initialization
 		super().__init__(
 			content=content, style=style,
 			parent_element=parent_element, children_elements=children_elements,
 			previous_element=previous_element, next_element=next_element
 		)
+
+	def _construct_text_from_content(self):
+		return ""
+
+
+class Paragraph(DirectedElement):
+	def __init__(
+			self,
+			# DirectedElement attributes
+			content: List[Content], style: str,
+			# Paragraph specific attributes
+			# DirectedElement attributes (with default)
+			parent_element: DirectedElement | None = None, children_elements: List[DirectedElement] | None = None,
+			previous_element: DirectedElement | None = None, next_element: DirectedElement | None = None
+			# Paragraph specific attributes (with default)
+	):
+		# DirectedElement initialization
+		super().__init__(
+			content=content, style=style,
+			parent_element=parent_element, children_elements=children_elements,
+			previous_element=previous_element, next_element=next_element
+		)
+
+	def _construct_text_from_content(self):
+		return ""
+
+
+class Table(DirectedElement):
+	def __init__(
+			self,
+			# DirectedElement attributes
+			content: List[Content], style: str,
+			# Table specific attributes
+			# DirectedElement attributes (with default)
+			parent_element: DirectedElement | None = None, children_elements: List[DirectedElement] | None = None,
+			previous_element: DirectedElement | None = None, next_element: DirectedElement | None = None
+			# Table specific attributes (with default)
+	):
+		# DirectedElement initialization
+		super().__init__(
+			content=content, style=style,
+			parent_element=parent_element, children_elements=children_elements,
+			previous_element=previous_element, next_element=next_element
+		)
+
+	def _construct_text_from_content(self):
+		return ""
--- enhanced_md/enhanced_md.py
+++ enhanced_md/enhanced_md.py
@@ -130,33 +130,42 @@ class EnhancedMD:
 		:return:
 		"""
 
-		paragraph = []
+		paragraph_content = []
 		#
 		for docx_paragraph_content in docx_paragraph.iter_inner_content():
 			# Only process paragraph contents which are not empty
 			if len(docx_paragraph_content.text):
 				# Detect whether paragraph content is run or hyperlink and process accordingly
 				if isinstance(docx_paragraph_content, docx.text.run.Run):
-					run = self._process_docx_run(docx_run=docx_paragraph_content)
+					run_content = self._process_docx_run(docx_run=docx_paragraph_content)
+
+					# Apply special paragraph_content concat
+					paragraph_content = self._concat_run_content_to_content_list(
+						content_list=paragraph_content,
+						run_content=run_content
+					)
 
 				elif isinstance(docx_paragraph_content, docx.text.hyperlink.Hyperlink):
-					print(repr(docx_paragraph_content.text))
+					paragraph_content.append(self._process_docx_hyperlink(docx_hyperlink=docx_paragraph_content))
 
-	def _process_docx_run(self, docx_run: docx.text.run.Run) -> List[enhanced_elements.Content]:
+		print(paragraph_content)
+
+	@staticmethod
+	def _process_docx_run(docx_run: docx.text.run.Run) -> List[enhanced_elements.Content]:
 		"""
 
 		:param docx_run:
-		:return processed_run:
+		:return run_content:
 		"""
 
-		# Split run text by spaces, sequences of word characters, and individual punctuation characters
-		pattern = r'(\s|[\w]+|\W)'
+		# Split run text by spaces, sequences of word characters ('-' included), and punctuation characters
+		pattern = r"(\s|[\w-]+|\W)"
 		content_split = re.findall(pattern, docx_run.text)
 
-		processed_run = []
+		run_content = []
 		#
 		for content in content_split:
-			processed_run.append(
+			run_content.append(
 				enhanced_elements.Content(
 					string=content,
 					# Font style attributes return either True or None
@@ -169,7 +178,82 @@ class EnhancedMD:
 				)
 			)
 
-		return processed_run
+		return run_content
+
+	def _process_docx_hyperlink(self, docx_hyperlink: docx.text.hyperlink.Hyperlink) -> enhanced_elements.Hyperlink:
+		"""
+
+		:param docx_hyperlink:
+		:return hyperlink:
+		"""
+
+		hyperlink_content = []
+		#
+		for docx_hyperlink_content in docx_hyperlink.runs:
+			run_content = self._process_docx_run(docx_run=docx_hyperlink_content)
+
+			# Apply special hyperlink_content concat
+			hyperlink_content = self._concat_run_content_to_content_list(
+				# Even though content_list can contain other hyperlinks, it will always consist of runs in this case
+				content_list=hyperlink_content,
+				run_content=run_content
+			)
+
+		return enhanced_elements.Hyperlink(
+			content=hyperlink_content, address=docx_hyperlink.address, fragment=docx_hyperlink.fragment
+		)
+
+	def _concat_run_content_to_content_list(
+			self,
+	        content_list: List[enhanced_elements.Content | enhanced_elements.Hyperlink],
+			run_content: List[enhanced_elements.Content]
+	) -> List[enhanced_elements.Content | enhanced_elements.Hyperlink]:
+		"""
+
+		:param content_list:
+		:param run_content:
+		:return content_list:
+		"""
+
+		# Only if content_list is not empty and previous content is content class
+		if len(content_list) and isinstance(content_list[-1], enhanced_elements.Content):
+			# Detect whether the special concat is needed
+			is_special_concat = self._detect_special_content_concat(
+				content_a=content_list[-1], content_b=run_content[0]
+			)
+		else:
+			is_special_concat = False
+
+		# Apply special concat if detected else normal concat
+		if is_special_concat:
+			# Join junction last content_list and first run_content strings
+			# (does not matter which content is chosen to define font style since they must be the same)
+			content_list[-1].string += run_content[0].string
+			content_list += run_content[1:]
+		else:
+			content_list += run_content
+
+		return content_list
+
+	@staticmethod
+	def _detect_special_content_concat(
+			content_a: enhanced_elements.Content, content_b: enhanced_elements.Content
+	):
+		"""
+
+		:param content_a:
+		:param content_b:
+		:return is_special_concat:
+		"""
+
+		return (
+			# Content A string ends with word character ('-' included)
+			bool(re.search(r"[\w-]$", content_a.string))
+			# Content B string begins with word character ('-' included)
+			and bool(re.search(r"^[\w-]", content_b.string))
+			# Last paragraph_content and first run_content have the same font style attributes
+			and content_a.font_style == content_b.font_style
+		)
 
 	def _process_docx_table(self, docx_table: docx.table):
 		pass
