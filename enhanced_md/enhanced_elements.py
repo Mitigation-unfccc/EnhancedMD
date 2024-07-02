a46659faf96f62567eeaa9c4e4c9ece20be2789e
Pau Tarragó Navarra <p.tarragonavarra@gmail.com>
2024-07-02 17:13:44 +0200

    .

--- enhanced_md/enhanced_elements.py
+++ enhanced_md/enhanced_elements.py
@@ -251,7 +251,7 @@ class DirectedElement(BaseElement):
         next_element.previous = self
 
     def construct_identifier_string(self) -> str:
-        return f"{'.'.join(map(str, [x+1 for x in self.item]))}{f' ({self.numbering})' if self.numbering is not None else ''}"
+        return f"{'.'.join(map(str, [x+1 for x in self.item]))}"
 
     def _has_numbering(self):
         num_id, ilvl = self._obtain_num_id_and_ilvl()
@@ -475,7 +475,7 @@ class Paragraph(DirectedElement):
             next_element.heading_item = self.heading_item
 
     def construct_identifier_string(self) -> str:
-        return (f"{' '.join(map(str, [x+1 for x in self.heading_item])) if self.heading_item is not None else 'NONE'}"
+        return (f"{'.'.join(map(str, [x+1 for x in self.heading_item])) if self.heading_item is not None else 'NONE'}"
                 f".{super().construct_identifier_string()}")
 
 
